5caaa3c3a681da9d269521beca43408c1288ae60
Attilius <leo20@freemail.hu>
2022-01-12 20:26:01 +0100

    I modified getId function

--- index.js
+++ index.js
@@ -823,7 +823,25 @@ const fillAttractionsArray = (databaseArray, responseArray, cityName) => {
 }
 
 const getId = () => {
-    const letters = ["A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z"];
+    const letters = [
+        "A", "a", "B", 
+        "b", "C", "c", 
+        "D", "d", "E", 
+        "e", "F", "f", 
+        "G", "g", "H", 
+        "h", "I", "i", 
+        "J", "j", "K", 
+        "k", "L", "l", 
+        "M", "m", "N", 
+        "n", "O", "o", 
+        "P", "p", "Q", 
+        "q", "R", "r", 
+        "S", "s", "T", 
+        "t", "U", "u", 
+        "V", "v", "W", 
+        "w", "X", "x", 
+        "Y", "y", "Z", 
+        "z"];
     let id = "";
 
     for (let i = 0; i < 12; i++) {
