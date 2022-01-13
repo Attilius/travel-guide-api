481fd11f503a0d18471d18c35b9e7cc37f765588
Attilius <leo20@freemail.hu>
2022-01-13 20:38:28 +0100

    Modified index.js

--- index.js
+++ index.js
@@ -382,7 +382,7 @@ const cities = [
             },
             {
                 name: "Centre de commerce mondial de Montréal",
-                img: "https://images.unsplash.com/photo-1626677332862-6055ed53a549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                img: "https://images.unsplash.com/photo-1545956082-0a33eee5b440?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             },
         ],
         hotelImages: [
