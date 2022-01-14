640243339cfdba31500096dd04f477ad4d42cf26
Attilius <leo20@freemail.hu>
2022-01-14 20:26:40 +0100

    Modified index.js

--- index.js
+++ index.js
@@ -986,8 +986,8 @@ const cities = [
         ],
         restaurantNames: [
             {
-                first: ["Ristorante","Pizzeria", "Cesare", "Cafe", "La Tradizione", "La Forchetta", "Bistrot", "Petite", "La Grande", "Le Chasseur", "Le Canard", "Paris"],
-                second: ["Ristorante", "Gourmandes", "Romana", "Alliance", "Fleur", "d'Oro", "Magique", "Tulipes"],
+                first: ["Ristorante","Pizzeria", "Cesare", "Cafe", "La Tradizione", "La Forchetta", "Cala", "Petite", "La Grande", "Le Chasseur", "Le Canard", "Romana"],
+                second: ["Ristorante", "Gourmandes", "Romana", "Alliance", "Luna", "d'Oro", "Magique", "Tulipes"],
                 third: ["Ristorante"],
                 fourth: ["Ristorante"]
             }
