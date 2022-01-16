1b4d3de0323ce0e1815b7eb9f1044c072ceed605
Attilius <leo20@freemail.hu>
2022-01-16 20:42:22 +0100

    Modified index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const cities = [
             },
             {
                 name: "Camel Pride",
-                img: "https://images.unsplash.com/photo-1590081159886-a60883672ebb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1004&q=80"
+                img: "https://images.unsplash.com/photo-1579349689360-0f41791ac8b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             },
             {
                 name: "The Mosque of Rifai and Sultan Hassan",
@@ -39,12 +39,12 @@ const cities = [
                 img: "https://images.unsplash.com/photo-1562679299-266edbefd6d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
             },
             {
-                name: "Kadiköy, Istanbul, Turkey",
-                img: "https://images.unsplash.com/photo-1559389864-340ac6773263?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                name: "Mosque of Muhammad Ali",
+                img: "https://images.unsplash.com/photo-1592996757998-43ef2e0b0835?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             },
             {
-                name: "Old Bazaar, Istanbul, Turkey",
-                img: "https://images.unsplash.com/photo-1526586860017-748c7c448ab6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                name: "Mortuary Temple Of Khufu",
+                img: "https://images.unsplash.com/photo-1566288623394-377af472d81b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
             },
             {
                 name: "The bridge of Istanbul connecting Asia and Europe.",
